a564a36158128e8be9996711a2ddc75d6402655c
Sankalpbp <sankalpnitj@gmail.com>
2022-11-11 04:29:43 +0530

    Deleting the first node in the list

---
+++ linkedLists/DeleteFirstNode.java
@@ -0,0 +1,35 @@
+public class DeleteFirstNode {
+
+    private static void printList ( Node head ) {
+
+        if ( head == null ) {
+            System.out.println ( "Empty List" );
+            return;
+        }
+
+        Node ptr = head;
+
+        while ( ptr != null ) {
+            System.out.print ( ptr.data );
+            if ( ptr.next != null ) {
+                System.out.print ( " -> " );
+            }
+            ptr = ptr.next;
+        }
+        System.out.println ();
+    }
+
+    public static Node deleteFirstNode ( Node head ) {
+        if ( head == null || head.next == null ) {
+            return null;
+        }
+
+        return head.next;
+    }
+
+    public static void main ( String... args ) {
+        Node ll = CreateLinkedList.createLinkedList ();
+        ll = deleteFirstNode ( ll );
+        printList ( ll );
+    }
+}
