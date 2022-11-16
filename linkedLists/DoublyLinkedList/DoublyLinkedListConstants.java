0aa0dbb1b70d3646a53e4a2167a6b7f96aa7e3aa
Sankalpbp <sankalpnitj@gmail.com>
2022-11-16 05:59:17 +0530

    Creating LinkedList constants file for clean code

---
+++ linkedLists/DoublyLinkedList/DoublyLinkedListConstants.java
@@ -0,0 +1,8 @@
+package linkedLists;
+
+public interface LinkedListConstants {
+
+    public static final String DATA_PROMPT_FOR_NEW_NODE = "Enter the data for the new Node: ";
+    public static final String DATA_PROMPT_FOR_HEAD_NODE = "Enter the data to be put in the head node: ";
+    public static final String PROMPT_FOR_MORE_DATA = "Do you want to continue ? (Y / N): ";
+}
